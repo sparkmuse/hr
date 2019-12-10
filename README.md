5581f988495ed18aa9b2f372ebad7e58a9f17e93
Alfredo Lopez <alfredlopezf@hotmail.com>
2019-12-10 19:10:28 +0100

    Finished OfferRepository
    Also I added the readme file

---
+++ README.md
@@ -0,0 +1,7 @@
+## Use cases
+- [x] user has to be able to create a job offer and read a single and list all offers. 
+- [ ] candidate has to be able to apply for an offer.
+- [ ] user has to be able to read one and list all applications per offer.
+- [ ] user has to be able to progress the status of an application.
+- [ ] user has to be able to track the number of applications.
+- [ ] status change triggers a notification (*)
--- src/test/java/com/heavenhr/challenge/repositories/OfferRepositoryTest.java
+++ src/test/java/com/heavenhr/challenge/repositories/OfferRepositoryTest.java
@@ -1,19 +1,17 @@
 package com.heavenhr.challenge.repositories;
 
 import com.heavenhr.challenge.entity.Offer;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.jdbc.core.JdbcTemplate;
 
 import javax.persistence.EntityManager;
 import java.time.LocalDate;
-import java.util.Arrays;
-import java.util.List;
+import java.util.Optional;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.assertj.core.api.Assertions.assertThat;
 
 
 @DataJpaTest
@@ -25,14 +23,20 @@ class OfferRepositoryTest {
     @Autowired
     private EntityManager entityManager;
 
-    @Test
-    @DisplayName("can create offer")
-    void canSave() {
+    private Offer offer;
 
-        Offer offer = Offer.builder()
+    @BeforeEach
+    void setUp() {
+        offer = Offer.builder()
                 .jobTitle("Java Developer")
                 .startDate(LocalDate.now())
                 .build();
+    }
+
+    @Test
+    @DisplayName("creates offer")
+    void canSave() {
+
         Offer savedOffer = offerRepository.save(offer);
 
         Offer found = entityManager
@@ -40,7 +44,31 @@ class OfferRepositoryTest {
                 .setParameter("id", savedOffer.getId())
                 .getSingleResult();
 
-        assertEquals(found, offer);
+        assertThat(found).isEqualToComparingFieldByField(offer);
     }
 
+    @Test
+    @DisplayName("gets a list of offers")
+    void getOffers() {
+
+        offerRepository.save(offer);
+
+        Iterable<Offer> offers = offerRepository.findAll();
+
+        assertThat(offers).containsExactly(offer);
+
+    }
+
+    @Test
+    @DisplayName("gets an offer by id")
+    void getOffer() {
+
+        Offer savedOffer = offerRepository.save(offer);
+        Optional<Offer> expected = Optional.of(savedOffer);
+
+        Optional<Offer> actual = offerRepository.findById(savedOffer.getId());
+
+        assertThat(actual).isEqualTo(expected);
+
+    }
 }
